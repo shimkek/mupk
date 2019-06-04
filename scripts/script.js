ec7347c3c5665aa3fc46dbb943dea88bf9aa86f6
shimkek <maksim.dunaitsev@gmail.com>
2019-06-05 03:50:34 +0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,77 @@
+let user1 = { 'name': 'Олег', 'surname': 'Пекан' };
+let user2 = { 'name': 'Андрей', 'surname': 'Грек' };
+let user3 = { 'name': 'Анвар', 'surname': 'Джобс' };
+let user4 = { 'name': 'Рома', 'surname': 'Метробумин' };
+let users = [user1, user2, user3, user4];
+let number = users.length;
+
+const addRow = (number, element, row) => {
+    const elementName = document.createTextNode(element.name);
+    const elementSurname = document.createTextNode(element.surname);
+    const elementNumber = document.createTextNode(number)
+
+    const dataName = document.createElement("td");
+    const dataSurname = document.createElement("td");
+    const dataNumber = document.createElement("td");
+
+    dataNumber.appendChild(elementNumber);
+    dataName.appendChild(elementName);
+    dataSurname.appendChild(elementSurname);
+
+    row.appendChild(dataNumber);
+    row.appendChild(dataName);
+    row.appendChild(dataSurname);
+    document.getElementById('table').appendChild(row);
+};
+
+
+const fillTable = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        let number = i + 1;
+        const element = array[i];
+        const row = document.createElement("tr");
+        addRow(number, element, row);
+    }
+
+};
+
+
+
+const addElement = (user) => {
+    number++;
+    const row = document.createElement("tr");
+
+    addRow(number, user, row);
+};
+
+
+const addNurbot = () => {
+
+    let user5 = { 'name': 'nurbot', 'surname': 'nazarbaev' };
+    addElement(user5);
+
+};
+
+
+const deleteNurtable = () => {
+    var elem = document.getElementById('nurtable');
+    elem.parentNode.removeChild(elem);
+
+};
+
+
+const createNurtable = () => {
+    number = 0;
+    var codeBlock = '<table class="table" id="nurtable"><thead><tr><th scope="col">Номер</th><th scope="col">Имя</th><th scope="col">Фамилия</th></tr></thead><tbody id="table"></tbody></table>';
+
+    document.getElementById("wrapper").innerHTML = codeBlock;
+};
+
+const addElementInput = () => {
+    const nameForm = document.getElementById("nameForm").value;
+    const surnameForm = document.getElementById("surnameForm").value;
+    const user = { 'name': nameForm, 'surname': surnameForm };
+    addElement(user);
+    document.getElementById("nameForm").value = "";
+    document.getElementById("surnameForm").value = "";
+};
